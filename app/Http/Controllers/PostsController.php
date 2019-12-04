a737018e4d070fe84db1d0f47c4ed6be645a3883
offiahchiaz <offiahchiaz@gmail.com>
2019-12-04 06:54:00 +0000

    Added PostsController and Post model files

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Post;
+use Auth;
+
+class PostsController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['show']]);
+    }
+
+    public function index()
+    {
+        $posts = Post::paginate(25);
+        return view('posts.index')->withPosts($posts);
+    }
+
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'title' => 'required|max:255',
+            'content' => 'required'
+        ]);
+
+        $user = Auth::user();
+
+        $post = $user->posts()->create([
+            'title' => $request->title,
+            'content' => $request->content,
+            'published' => $request->has('published')
+        ]);
+
+        return redirect()->route('posts.show', $post->id);
+    }
+
+    public function show($id)
+    {
+      $post = Post::findOrFail($id);
+      return view('posts.show')->withPost($post);
+    }
+
+    public function edit($id)
+    {
+      $post = Post::findOrFail($id);
+      return view('posts.edit')->withPost($post);
+    }
+
+    public function update(Request $request, $id)
+    {
+      $this->validate($request, [
+        'title' => 'required|max:255',
+        'content' => 'required',
+      ]);
+
+      $post = Post::findOrFail($id);
+      $post->title = $request->title;
+      $post->content = $request->content;
+      $post->published = ($request->has('published') ? true : false);
+      $post->save();
+
+      return redirect()->route('posts.show', $post->id);
+    }
+
+    public function destroy($id)
+    {
+      Post::destroy($id);
+    }
+}
